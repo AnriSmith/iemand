eb51302228b43f29a9d4a05a9a68665059cb446d
AnriS <smtanr001@myuct.ac.za>
2018-03-16 13:55:14 +0200

    travis

--- R/creat_age.R
+++ R/creat_age.R
@@ -1,11 +1,12 @@
 
-#' Introduction to R packages
+#' Introduction to R packages; creating a Fake People
 #'
 #' @return Return a value for your age
 #' @export
 #'
-#' @examples
 create_age <- function(){
   age <- sample(1:100, size = 1)
   return(age)
 }
+
+
