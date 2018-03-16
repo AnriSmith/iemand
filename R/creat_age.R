48e0a9e13cb6cb9d143c33baecb845a1728b31e1
AnriS <smtanr001@myuct.ac.za>
2018-03-16 10:58:34 +0200

    firt commit

---
+++ R/creat_age.R
@@ -0,0 +1,14 @@
+
+#' Introduction to R packages
+#'
+#' @return Return a value for your age
+#' @export
+#'
+#' @examples
+create_age <- function(){
+  age <- sample(1:100, size = 1)
+  return(age)
+}
+
+
+use_git()
